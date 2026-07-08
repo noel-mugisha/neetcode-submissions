2bee41d8de5cd789e5b78248eb6d38f26910bed0
Noel <noelmugisha332@gmail.com>
2026-07-08 11:45:38 +0200

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    ListNode merge(ListNode[] lists, int start, int end) {
+        if (start >= end) {
+            return lists[start];
+        }
+
+        int mid = start + (end - start) / 2;
+        ListNode l1 = merge(lists, start, mid);
+        ListNode l2 = merge(lists, mid + 1, end);
+        
+        return sortNodes(l1, l2);
+    }
+
+    ListNode sortNodes(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        if (l1 != null)
+            curr.next = l1;
+        if (l2 != null)
+            curr.next = l2;
+        
+        return dummy.next;
+    }
+}
